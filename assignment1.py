66353adbdf3231f73a0e4e734e096a58442627d0
mkanias <131133498+mkanias@users.noreply.github.com>
2024-08-28 11:56:15 +1000

    Wrote down the template code and type hinted it.

---
+++ assignment1.py
@@ -0,0 +1,6 @@
+class CityMap:
+    def __init__(self, roads: list[tuple], tracks: list[tuple], friends: list[tuple]) -> None:
+        pass
+
+    def plan(self, start: int, destination: int) -> tuple: #(total_time, route, pickup_friend, pickup_location)
+        pass
