59b40d6c3f10b8930eda7dd27f56406b81210b14
mkanias <131133498+mkanias@users.noreply.github.com>
2024-08-29 11:47:33 +1000

    Created the dijkstra method which finds the shortest path and can take in optional end_node parameter and optional edge type parameter.

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,8 @@
-
+from minheap import MinHeap
 
 
 class CityMap:
-    def __init__(self, roads: list[tuple[int]], tracks: list[tuple[int]], friends: list[tuple[int,str]]) -> None:
+    def __init__(self, roads: list[tuple[int,int,int]], tracks: list[tuple[int,int,int]], friends: list[tuple[int,str]]) -> None:
         # define the roads, tracks and friends inputs as class variables 
         self.roads = roads
         self.tracks = tracks
@@ -23,7 +23,7 @@ class CityMap:
     def add_roads(self) -> None:
         # looping through each road tuple
         for road in self.roads:
-            (start_vertex, end_vertex, time) = road
+            start_vertex, end_vertex, time = road
 
             self.adj_list[start_vertex].append((end_vertex, time, "R")) # adding edge btw vertices and identfying that it's a road
             self.adj_list[end_vertex].append((start_vertex, time, "R")) # adding the opposite way coz its undirected
@@ -34,7 +34,51 @@ class CityMap:
         for track in self.tracks:
             (start_vertex, end_vertex, time) = track
 
-            self.adj_list[start_vertex].append((end_vertex, time, "T")) # adding edge btw vertices and identfying that it's a road
+            self.adj_list[start_vertex].append((end_vertex, time, "T")) # adding edge btw vertices and identfying that it's a track which is one directional
+
+
+
+    def dijkstras(self, start_node: int, end_node: int = None, edge_type_input: str = "R"): # optional end node parameter and edge_type
+        # initialise distance and parent structues
+        # the * method works bc the list items that are being initialised are immutable and therefore they are all independent copies
+        distance = [float('inf')] * len(self.adj_list) # all distances are infinity
+        parent = [-1] * len(self.adj_list) # all parents are -1
+
+        distance[start_node] = 0 # the distance from the start vertex to itself is 0
+
+        # initialise the priority queue with all vertices
+        priority_queue = MinHeap()
+        priority_queue.insert(start_node, 0)
+
+        while not priority_queue.is_empty():
+            u, dist_u = priority_queue.extract_min() # extract vertex with minimum distance
+
+            # stop if the end node is reached because thats all we need to traverse
+            if end_node is not None and u == end_node:
+                break
+
+
+            # relaxation step
+            for v, weight, edge_type in self.adj_list[u]: 
+                if edge_type == edge_type_input: # making sure to traverse the roads and tracks separately
+                    if distance[u] + weight < distance[v]:
+                        distance[v] = distance[u] + weight
+                        parent[v] = u
+                        priority_queue.insert(v, distance[v])
+
+        # reconstructing shortest path if end node is provided
+        if end_node is not None:
+            path = []
+            current = end_node
+            while current != -1:
+                path.append(current)
+                current = parent[current]
+            path.reverse()
+            return distance[end_node], path
+
+        # if not, then just return the distance and parent lists
+        return distance, parent
+
 
 
     def __str__(self) -> str:
@@ -45,16 +89,16 @@ class CityMap:
         return "\n".join(result)
 
 
-    def plan(self, start: int, destination: int) -> tuple: #(total_time, route, pickup_friend, pickup_location)
+    def plan(self, start: int, destination: int) -> tuple: # (total_time, route, pickup_friend, pickup_location)
         pass
 
 
 
-
 if __name__ == "__main__":
-    roads = [(0,1,4), (0,3,2), (2,0,3), (3,1,2), (2,4,2), (4,5,3), (4,3,2)]
+    roads = [(0,1,4), (0,3,2), (2,0,3), (3,1,2), (2,4,2), (4,5,3)]
     tracks = [(1,3,3), (3,4,2), (4,3,2), (4,5,4), (5,1,6)]
     cm = CityMap(roads,tracks,friends=[])
-   
-    print(cm)
-    
+
+    print(cm.dijkstras(start_node=1))
+
+
