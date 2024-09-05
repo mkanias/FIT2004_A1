eec0567d4536cffdcecdba82cac87640277b308f
mkanias <131133498+mkanias@users.noreply.github.com>
2024-09-05 10:54:04 +1000

    Complexity decrease
    - Decreased the space complexity of the init by not using a bfs, but instead checking if the track locations have a friend there and if so incrementing the connecting node by 1.

--- graph.py
+++ graph.py
@@ -1,65 +1,47 @@
-from collections import deque
-
 
 class Graph:
     def __init__(self, roads, tracks, friends) -> None:
+        self.roads = roads
+        self.tracks = tracks
+        self.friends = friends
+
         size_initialisation = len(roads) + 1
 
         self.road_graph = [[[], (None, float('inf'), float('inf'))] for _ in range(size_initialisation)] 
-        self.track_graph = [[[], (None, float('inf'), float('inf'))] for _ in range(size_initialisation)]
 
-        self.add_edges_undirected(roads)
-        self.add_edges_directed(tracks)
-        self.multi_source_bfs(friends)
+        self.add_edges_undirected()
+        self.add_friend_locations()
         self.filter_graph()
 
 
-    def add_edges_undirected(self, edge_list) -> None:
-        for edge in edge_list:
+    def add_edges_undirected(self) -> None:
+        for edge in self.roads:
             start_vertex, end_vertex, weight = edge
 
             self.road_graph[start_vertex][0].append((start_vertex, end_vertex, weight))
             self.road_graph[end_vertex][0].append((end_vertex, start_vertex, weight))
 
     
-    def add_edges_directed(self, edge_list) -> None:
-        for edge in edge_list:
-            (start_vertex, end_vertex, weight) = edge
-
-            self.track_graph[start_vertex][0].append(edge) 
-
-
-    def multi_source_bfs(self, friends):
-        queue = deque()
-        visited = [False] * len(self.track_graph)
-
-        # Initialize the BFS queue with all friend locations
-        for friend, location in friends:
-            queue.append((location, 0))  # (node, hops)
-            visited[location] = True
+    def add_friend_locations(self) -> None:
+        for friend, location in self.friends:
             self.road_graph[location][1] = (friend, location, 0)  # Update the friend's own location
 
-        # Perform BFS
-        while queue:
-            current_node, hops = queue.popleft()
+        for edge in self.tracks:
+            (start_vertex, end_vertex, weight) = edge
 
-            # Stop if we've reached the hop limit
-            if hops == 2:
-                continue
+            friend_name = self.road_graph[start_vertex][1][0]
+            start_pickup_hops = self.road_graph[start_vertex][1][2]
+            end_pickup_hops = self.road_graph[end_vertex][1][2]
 
-            # Explore neighbors
-            for _, neighbor, weight in self.track_graph[current_node][0]:
-                if not visited[neighbor]:
-                    visited[neighbor] = True
-                    queue.append((neighbor, hops + 1))
-                    if hops + 1 < self.road_graph[neighbor][1][2]:
-                        self.road_graph[neighbor][1] = (self.road_graph[current_node][1][0], neighbor, hops + 1)
+            if (friend_name is not None) and (start_pickup_hops < end_pickup_hops) and (start_pickup_hops + 1 <= 2):
+                self.road_graph[end_vertex][1] = (friend_name, end_vertex, start_pickup_hops + 1)
 
 
     def filter_graph(self):
         empty_element = self.road_graph[-1][0]
         if not empty_element:
             self.road_graph.pop()
+
 
     def get_graph(self):
         return self.road_graph
@@ -76,13 +58,12 @@ class Graph:
         return "\n".join(result)
 
 if __name__ == "__main__":
-    roads = [(0,1,4), (0,3,2), (2,0,3), (3,1,2), (2,4,2), (4,5,3)]
-    tracks = [(1,3,3), (3,4,2), (4,3,2), (4,5,4), (5,1,6)]
-    friends = [("Grizz", 1), ("Ice", 3)]    
+    roads = [(0,1,4), (0,3,2), (2,0,3), (3,1,2), (2,4,2), (2,5,2)]
+    tracks = [(1,3,4), (3,4,2), (4,5,1), (5,1,6)]
+    friends = [("Grizz", 1)]
 
     graph = Graph(roads, tracks, friends) # initialising the space for the adj_list O(|R|)
 
-    graph.multi_source_bfs(friends)
     print(graph)
 
     
